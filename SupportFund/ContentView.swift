8a833fae6286f81c01f62c33556fb9b6fe666708
najak2007 <najak2007@gmail.com>
2026-05-18 20:57:01 +0900

    Init

---
+++ SupportFund/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SupportFund
+//
+//  Created by 오션블루 on 5/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SupportFund/SupportFundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupportFundApp.swift
+//  SupportFund
+//
+//  Created by 오션블루 on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SupportFundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
